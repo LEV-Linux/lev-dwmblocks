fb6c431b7b5964deb81ae8964397e81fc4be8e29
salahdin-ahmed <salah2112004@gmail.com>
2021-01-06 15:46:51 +0200

    Intial configuration

--- blocks.def.h
+++ blocks.def.h
@@ -2,15 +2,14 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
-	
-	{"│ 📡", "status_internet",			5,		3},
-	{"│ ", "status_volume",  			0,		1},
-	{"│ 🔋 ", "status_battery",			30,		0},
-	{"","status_battery_update",			1,		0},
-	{"│ 💽 ", "status_disk",			3600,		2},
-	{"│  ", "status_cpu",				1,		0},
-	{"│ 💿 ", "status_ram",				1,		0},
-	{"│ ", "status_date",				1,		0},
+	{"",		"status_recording",		0,		5},
+	{"",		"status_internet",		5,		3},
+	{"│ ",		"status_volume",  		0,		1},
+	{"│ 🔋 ",	"status_battery",		2,		0},
+	{"│ 💽 ",	"status_disk",			3600,		2},
+	{"│  ",	"status_cpu",			1,		0},
+	{"│ 💿 ",	"status_ram",			1,		0},
+	{"│ ",		"status_date",			1,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
